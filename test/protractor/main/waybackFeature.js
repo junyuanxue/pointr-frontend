13ef06a3a2da80372c24bef87ca1485c43db6dce
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-07 17:54:01 +0100

    created first feature test

---
+++ test/protractor/main/waybackFeature.js
@@ -0,0 +1,10 @@
+describe('Wayback', function () {
+  it('creates a new journey', function () {
+    browser.get('/');
+    $('#create-journey').click();
+    var addWaypointButton = $('#add-waypoint');
+    var endJourneyButton = $('#end-journey');
+    expect(addWaypointButton.getText()).toEqual('Add Waypoint');
+    expect(endJourneyButton.getText()).toEqual('End Journey');
+  })
+})
