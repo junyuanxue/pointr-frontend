d9e992a281070fe608b3f14e42453e4f283952d6
Junyuan Xue <junyuanxue.is@gmail.com>
2016-05-07 18:15:38 +0100

    running first feature test

--- test/protractor/main/waybackFeature.js
+++ test/protractor/main/waybackFeature.js
@@ -1,10 +1,13 @@
+'use strict';
+
 describe('Wayback', function () {
   it('creates a new journey', function () {
     browser.get('/');
     $('#create-journey').click();
     var addWaypointButton = $('#add-waypoint');
     var endJourneyButton = $('#end-journey');
+    expect(browser.getTitle()).toEqual('Journey');
     expect(addWaypointButton.getText()).toEqual('Add Waypoint');
     expect(endJourneyButton.getText()).toEqual('End Journey');
-  })
-})
+  });
+});
