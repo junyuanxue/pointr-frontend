a26e607e9496b555072fcb7258c590139c72c163
Tobenna <tobennandu@gmail.com>
2016-05-10 16:57:01 +0100

    MVP BEBE

--- app/main/controllers/allJourneysController.js
+++ app/main/controllers/allJourneysController.js
@@ -5,7 +5,9 @@ angular
   .controller('AllJourneysController', ['$scope','JourneyFactory', 'JourneyService', '$location',
     function ($scope, JourneyFactory, JourneyService, $location) {
         $scope.getAllJourneys = function () {
+          console.log("HI");
           JourneyService.getAllJourneys().then(function (journeys) {
+            console.log(journeys);
             if (typeof journeys !== 'undefined' ) {
               $scope.allJourneys = journeys;
               console.log($scope.allJourneys);
@@ -16,6 +18,5 @@ angular
         $scope.loadJourney = function (id) {
           $location.path('/main/journeyback/' + id);
         };
-
         $scope.allJourneys = $scope.getAllJourneys();
 }]);
