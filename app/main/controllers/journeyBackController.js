ddb5089feae49c77b947806e5e6979016cd6972b
Murilo Dal Ri <murilodalri@gmail.com>
2016-05-12 11:13:52 +0100

    private functions

--- app/main/controllers/journeyBackController.js
+++ app/main/controllers/journeyBackController.js
@@ -53,7 +53,7 @@ angular
           $scope.currentLocation = currentLocation;
           _displayCenter();
           _centerStyling();
-          if (isCloseEnoughToWaypoint()) { changeCurrentWaypoint(); }
+          if (_isCloseEnoughToWaypoint()) { _changeCurrentWaypoint(); }
           $scope.distanceFromWaypoint = _displayDistanceBetween(currentLocation, $scope.currentWaypoint.coords);
         });
       }
@@ -85,28 +85,30 @@ angular
       }
 
       function getFirstWaypoint () {
-        reverseWaypoints();
+        _reverseWaypoints();
         return $scope.journey.waypoints[0];
       }
 
-      function reverseWaypoints () {
+      function _reverseWaypoints () {
         $scope.journey.waypoints = $scope.journey.waypoints.reverse();
       }
 
-      function isCloseEnoughToWaypoint () {
+      function _isCloseEnoughToWaypoint () {
         return distanceBetween($scope.currentLocation, $scope.currentWaypoint.coords) < 5;
       }
 
-      function changeCurrentWaypoint () {
+      function _changeCurrentWaypoint () {
         var currentWaypointIndex = $scope.journey.waypoints.indexOf($scope.currentWaypoint);
         _onWaypointReached();
         markAsReached($scope.journey.waypoints[currentWaypointIndex]);
-        if (currentWaypointIndex > - 1 && (currentWaypointIndex !== ($scope.journey.waypoints.length - 1))) {
+        if (_validateWaypoint()) {
           $scope.currentWaypoint = $scope.journey.waypoints[currentWaypointIndex + 1];
         }
       }
 
-      // function _
+      function _validateWaypoint (currentWaypointIndex) {
+        return currentWaypointIndex > - 1 && (currentWaypointIndex !== ($scope.journey.waypoints.length - 1));
+      }
 
       function _onWaypointReached () {
         $scope.currentWaypoint.icon = {url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'};
